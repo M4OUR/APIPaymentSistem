4a62cd903e42803d69d4180ebe0dcd802012e3cd
ychik <ychik1997@yandex.ru>
2025-03-05 20:40:42 +0300

    Обновлены настройки для развертывания на Vercel

--- manage.py
+++ manage.py
@@ -1,14 +1,19 @@
 import os
 import sys
 from dotenv import load_dotenv
+from django.core.wsgi import get_wsgi_application  # Импортируем WSGI-приложение
 
 # Загружаем переменные окружения из .env
 load_dotenv(os.path.join(os.path.dirname(__file__), '.env'))
 
+# Настройка для Django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
+
+# Точка входа для Vercel
+application = get_wsgi_application()  # Применяем переменную `application`
+
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
-
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
